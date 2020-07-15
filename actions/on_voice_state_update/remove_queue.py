78216b8417da7f6ceea3aebdddaf98f5057aada3
leet <therealbarryplayer@gmail.com>
2020-07-14 23:24:01 -0400

    added automatic queue removal

---
+++ actions/on_voice_state_update/remove_queue.py
@@ -0,0 +1,17 @@
+"""Removes unnecessary queues from the music manager"""
+
+import asyncio
+import logging
+import traceback
+
+import discord
+
+from utility import music
+
+
+async def run(client: discord.Client, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    if client.user.id == member.id:
+        if after.channel is None:
+            music_manager: music.MusicManager = client.music_manager
+            del music_manager.queues[member.guild.id]
+            logging.info(f"Removed music manager from {str(member.guild)}")
